588a01d1614d7d972ea1ecaab011b16dd0f87dfc
Markus Kurtz <kurtz@mathematik.uni-kl.de>
2023-06-20 14:58:05 +0200

    Add endomorphism algebra notes

--- src/EndomorphismRing.jl
+++ src/EndomorphismRing.jl
@@ -1,4 +1,4 @@
-function modularized_center_of_endomorphism_ring(M::Mod)
+function modularized_center_of_endomorphism_ring(M::Hecke.ModAlgAss)
     @req characteristic(base_ring(M)) == 0
     m = rand_prime()
     M_mod_m = M % m
@@ -12,5 +12,19 @@ function modularized_center_of_endomorphism_ring(M::Mod)
         m *= p
         done, c = rational_reconstruction(p, c_mod_m)
     end
+
+end
+
+function center_of_endomorphism_ring(M::Hecke.ModAlgAss{<:FinField})
+    # p = characteristic(base_ring(M))
+    cs = Hecke.composition_series(M)
+    # cs[i] ⊆ cs[j] for i≤j
+    # cs[i] generates submodule, ie. ∀ m ∈ action(M): cs[i] * m == x * cs[i]
+    # ∄ cs[i] ⊊ a ⊊ cs[i+1]: a generates submodule
+    # s[i] ≔ cs[i] / cs[i-1]
+    # Wie sieht Hom_A(S,T) für S≅T aus? → fixiere s∈S; Schiefkörper {φₘ : m∈T} mit φₘ(sa)=ma.
+    # Wie erkennen wir, ob S≅T? Vielleicht, ob obige Menge funktioniert?
+    # Wie bauen wir die Hom_A(S,T) zusammen? → siehe Notizen
+
 
 end
